c14b7bdab77387c68541e0b5f96c0540411a203e
My name <me@example.com>
2024-02-27 14:08:19 -0800

    project client relationship

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,17 @@ const {projects, clients } = require('../sampleData.js')
 
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
 
+//Project Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString},
+        description: { type: GraphQLString},
+        status: { type: GraphQLString}
+    }),
+});
+
 //Client Type
 const ClientType = new GraphQLObjectType({
     name: 'Client',
@@ -9,13 +20,32 @@ const ClientType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString},
         email: { type: GraphQLString},
-        phone: { type: GraphQLString}
+        phone: { type: GraphQLString},
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.find(client => client.id === parent.clientId)
+            }
+        }
     })
 });
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return projects.find((project) => project.id === args.id);
+            },
+        },
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
@@ -27,9 +57,9 @@ const RootQuery = new GraphQLObjectType({
             args: { id: {type: GraphQLID}},
             resolve(parent, args) {
                 return clients.find(client => client.id === args.id);
-            }
-        }
-    }
+            },
+        },
+    },
 });
 
 module.exports = new GraphQLSchema({
