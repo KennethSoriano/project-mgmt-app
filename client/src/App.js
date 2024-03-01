1e4b0388dcd032569bb771e631f4638bddc1aac9
My name <me@example.com>
2024-02-29 21:38:14 -0800

    React Router & pages setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import Clients from "./components/Clients";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import AddClientModal from "./components/AddClientModule";
-import Projects from "./components/Projects";
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -33,12 +33,15 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+          <Router>
+            <Header />
+            <div className="container">
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            </div>
+          </Router>
       </ApolloProvider>
     </>
   );
